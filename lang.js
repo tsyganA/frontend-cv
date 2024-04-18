7fbbd82cb132ba27643bd7344d811ca30f1bc297
Anton Tsyhan <anton.tsyhan@uco.co.il>
2024-04-18 17:05:46 +0300

    start cv

---
+++ lang.js
@@ -0,0 +1,11 @@
+const langArr = {
+  unit: {
+    en: 'Anton Tsyhan',
+    ru: 'Антон Цыган',
+  },
+  chip: {
+    ru: 'графический чип',
+    en: 'graphics processing unit',
+    ua: 'графічний чіп',
+  },
+};
